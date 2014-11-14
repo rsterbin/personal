b0329e07449aefc4f1ae8b63433a12e4a1e64ebd
Reha Sterbin <reha@omniti.com>
2014-11-14 20:14:12 +0000

    Make sure we don't choke on missing .bash_profile or .bashrc

--- init.sh
+++ init.sh
@@ -9,13 +9,12 @@ git submodule init
 git submodule update
 cp $DIR/gitconfig ~/.gitconfig
 $DIR/swap_vim.sh
-mv ~/.bash_profile ~/.old_bash_profile
+mv ~/.bash_profile ~/.old_bash_profile 2>/dev/null
 cp $DIR/bash_profile ~/.bash_profile
-mv ~/.bashrc ~/.old_bashrc
+mv ~/.bashrc ~/.old_bashrc 2>/dev/null
 cp $DIR/bashrc ~/.bashrc
 vim ~/.bash_profile ~/.old_bash_profile ~/.bashrc ~/.old_bashrc
-rm ~/.old_bash_profile ~/.old_bashrc
+rm ~/.old_bash_profile ~/.old_bashrc 2>/dev/null
 mkdir ~/.subversion 2>/dev/null
 cp $DIR/subversion_config ~/.subversion/config
-source ~/.bash_profile
 
