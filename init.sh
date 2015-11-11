fc7578ad6bf5a7811ac0ff86bb3e4ff6b9c2ac4e
Reha Sterbin <reha@omniti.com>
2015-11-11 10:19:57 -0500

    OmniOS path stuff to a new dot file

--- init.sh
+++ init.sh
@@ -11,10 +11,12 @@ cp $DIR/gitconfig ~/.gitconfig
 $DIR/swap_vim.sh
 mv ~/.bash_profile ~/.old_bash_profile 2>/dev/null
 cp $DIR/bash_profile ~/.bash_profile
+mv ~/.profile ~/.old_profile 2>/dev/null
+cp $DIR/profile ~/.profile
 mv ~/.bashrc ~/.old_bashrc 2>/dev/null
 cp $DIR/bashrc ~/.bashrc
-vim ~/.bash_profile ~/.old_bash_profile ~/.bashrc ~/.old_bashrc
-rm ~/.old_bash_profile ~/.old_bashrc 2>/dev/null
+vim ~/.bash_profile ~/.old_bash_profile ~/.profile ~/.old_profile ~/.bashrc ~/.old_bashrc
+rm ~/.old_bash_profile ~/.old_profile ~/.old_bashrc 2>/dev/null
 mkdir ~/.subversion 2>/dev/null
 cp $DIR/subversion_config ~/.subversion/config
 
