2cfb675459a5ac83917f58e79481b08cfd155856
Reha Sterbin <reha@omniti.com>
2016-08-02 10:00:52 -0400

    Iterm2 color check

--- shellconf/detect.sh
+++ shellconf/detect.sh
@@ -3,6 +3,7 @@
 # Check to see if we can use colors
 case "$TERM" in
     xterm-color) personal_can_do_color=yes;;
+    xterm-256color) personal_can_do_color=yes;;
 esac
 if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
     personal_can_do_color=yes
