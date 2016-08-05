bf80bc44d4afdd631070df51b6ecc41f8488e616
Reha Sterbin <reha@omniti.com>
2016-08-05 16:47:42 -0400

    Added handling for svn branches

--- shellconf/prompt.sh
+++ shellconf/prompt.sh
@@ -12,23 +12,36 @@ if [ "$personal_can_do_color" = yes ]; then
     PROMPT_CLOSE_COLOR=$(tput sgr0)
 fi
 
-# Git branch, colored to indicate state
+# Git or SVN branch, colored to indicate state
 # Based on https://gist.github.com/47267
-prompt_color_git_branch() {
+prompt_color_repo_branch() {
   git rev-parse --git-dir &> /dev/null
   git_status="$(git status 2> /dev/null)"
+  svn_lines=$(svn st 2> /dev/null | tr ";" "\n")
+  svn_info="$(svn info 2> /dev/null)"
   branch_pattern="On branch ([^${IFS}]*)"
   nobranch_pattern="Not currently on any branch."
   remote_pattern="Your branch is (.*) of"
   diverge_pattern="Your branch and (.*) have diverged"
+  svn_branch_pattern=$'URL: [^\n]*/branches/([^/\n]*)'
+  svn_trunk_pattern=$'URL: [^\n]*/trunk\n'
+  svn_other_pattern=$'URL: [^\n]*/([^/\n]*)'
+  svn_allowed_prefixes="^((X)|(Performing status on external item)) "
 
   # Clean: green
   color="${PROMPT_GREEN}"
 
   # Any changes? Red.
-  if [[ ! ${git_status} =~ "working directory clean" ]]; then
-      color="${PROMPT_RED}"
+  if [[ ! ${git_status} == "" ]]; then
+      if [[ ! ${git_status} =~ "working directory clean" ]]; then
+          color="${PROMPT_RED}"
+      fi
   fi
+  for line in $svn_lines; do
+      if [[ ! ${line} =~ ${svn_allowed_prefixes} ]]; then
+          color="${PROMPT_RED}"
+      fi
+  done
 
   # Ahead/behind/diverged: yellow
   if [[ ${git_status} =~ ${remote_pattern} ]]; then
@@ -45,6 +58,15 @@ prompt_color_git_branch() {
   fi
   if [[ ${git_status} =~ ${nobranch_pattern} ]]; then
       branch="submodule"
+  fi
+  if [[ ${svn_info} =~ ${svn_branch_pattern} ]]; then
+      branch=${BASH_REMATCH[1]}
+  fi
+  if [[ ${svn_info} =~ ${svn_trunk_pattern} ]]; then
+      branch="trunk"
+  fi
+  if [[ ${svn_info} =~ ${svn_other_pattern} ]]; then
+      branch=${BASH_REMATCH[1]}
   fi
   if [[ ${branch} != "none" ]]; then
       echo -e " \001${color}\002${branch}\001${PROMPT_CLOSE_COLOR}\002"
@@ -92,14 +114,14 @@ fi
 # Prompt
 if [[ $OPEN_COLOR != "" && $personal_can_do_color == 'yes' ]]; then
     if [ "$personal_uses_git" = yes ]; then
-        PS1="${OPEN_COLOR}$? [\u@${PS_HOST} \w${CLOSE_COLOR}\$(prompt_color_git_branch)${OPEN_COLOR}]$ ${CLOSE_COLOR}"
+        PS1="${OPEN_COLOR}$? [\u@${PS_HOST} \w${CLOSE_COLOR}\$(prompt_color_repo_branch)${OPEN_COLOR}]$ ${CLOSE_COLOR}"
     else
         PS1="${OPEN_COLOR}$? [\u@${PS_HOST} \w]$ ${CLOSE_COLOR}"
     fi
 else
     if [ "$personal_uses_git" = yes ]; then
         if [[ $personal_can_do_color == 'yes' ]]; then
-            PS1="$? [\u@${PS_HOST} \w\$(prompt_color_git_branch)]$ "
+            PS1="$? [\u@${PS_HOST} \w\$(prompt_color_repo_branch)]$ "
         else
             PS1="$? [\u@${PS_HOST} \w\$(prompt_git_branch)]$ "
         fi
