5cd23caf3227f8399a4267b2ad7784fad436e5b1
Reha Sterbin <reha@omniti.com>
2020-06-11 15:16:19 -0400

    new installations and paths

--- shellconf/macosx.sh
+++ shellconf/macosx.sh
@@ -2,8 +2,11 @@
 
 if [[ $personal_os == "osx" ]]; then
 
+    # Add PHP7 to the path
+    export PATH=/usr/local/php5-7.3.8-20190811-205217/bin:$PATH
+
     # Add Postgres.app command line tools
-    export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+    export PATH=/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH
 
     # Add Go to the path
     export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
@@ -13,6 +16,15 @@ if [[ $personal_os == "osx" ]]; then
 
     # Alias pngcrush
     alias pngcrush=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/pngcrush
+
+    # Add Scala to the path
+    export PATH=$PATH:$HOME/Source/scala-2.13.1/bin
+
+    # Add ansible-dk
+    alias ansible-mode='eval "$(ansible-dk shell-init bash)"; export PS1="(ADK) $PS1"'
+
+    # Boring no-cows :(
+    export ANSIBLE_NOCOWS=1
 
 fi
 
