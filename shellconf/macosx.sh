7c0a4afc3240823980cb22886414f7bd4bd4261b
Reha Sterbin <rsterbin@netapp.com>
2025-04-01 17:20:03 -0400

    Removed old pyenv stuff I'm not using anymore

--- shellconf/macosx.sh
+++ shellconf/macosx.sh
@@ -5,8 +5,8 @@ if [[ $personal_os == "osx" ]]; then
     # Add Applications to the path
     export PATH=/Applications:$PATH
 
-    # Add PHP7 to the path
-    export PATH=/usr/local/php5-7.3.8-20190811-205217/bin:$PATH
+    # Add Homebrew to the path
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
     # Add Postgres.app command line tools
     export PATH=/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH
@@ -14,22 +14,13 @@ if [[ $personal_os == "osx" ]]; then
     # Add Go to the path
     export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 
-    # Add Elastic Beanstalk to the path
-    export PATH=$PATH:$HOME/.ebcli-virtual-env/executables
-
-    # Alias pngcrush
-    alias pngcrush=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/pngcrush
-
-    # Add Scala to the path
-    export PATH=$PATH:$HOME/Source/scala-2.13.1/bin
-
     # Add ansible-dk
     alias ansible-mode='eval "$(ansible-dk shell-init bash)"; export PS1="(ADK) $PS1"'
 
     # Boring no-cows :(
     export ANSIBLE_NOCOWS=1
 
-    # Catalina needs to calm down about zsh
+    # OSX needs to calm down about zsh
     export BASH_SILENCE_DEPRECATION_WARNING=1
 
     # NVM
@@ -38,15 +29,7 @@ if [[ $personal_os == "osx" ]]; then
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
     # PyEnv
-    export WORKON_HOME=~/.virtualenvs
-    mkdir -p $WORKON_HOME
     export PATH=$(pyenv root)/shims:$PATH
-    export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
-    eval "$(pyenv init -)"
-    pyenv virtualenvwrapper_lazy
-
-    # PipEnv
-    export PATH=$HOME/.local/bin:$PATH
 
     # iTerm2 custom stuff
     if [[ $personal_use_iterm2 == yes ]]; then
@@ -54,18 +37,6 @@ if [[ $personal_os == "osx" ]]; then
         . $DIR/shellconf/iterm2/iterm2-tab-color.sh
         . $DIR/shellconf/iterm2/shortcuts.sh
     fi
-
-    # C2S search
-    plumsearch() {
-        search=$1
-        current=`pwd | xargs basename`
-        if [[ $current == "automator" || $current == "amz-edi" ]]; then
-            for dir in edi core web shop daemon base; do grep -rn "$search" "$dir/src"; done
-        fi
-        if [[ $current == "plumdash" ]]; then
-            grep -rn "$search" app
-        fi
-    }
 
 fi
 
